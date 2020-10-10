65f6101387bb107d5abb36275f083350c929bcd5
Ernest <perkowski.ernest98@gmail.com>
2020-10-10 15:40:55 +0200

    refactor(home): refactor name to subreddit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { SubredditComponent } from './components/subreddit/subreddit.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostItemComponent } from './components/post-item/post-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    SubredditComponent,
     PostsComponent,
     PostItemComponent
   ],
--- src/app/components/subreddit/subreddit.component.spec.ts
+++ src/app/components/subreddit/subreddit.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { SubredditComponent } from './subreddit.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SubredditComponent', () => {
+  let component: SubredditComponent;
+  let fixture: ComponentFixture<SubredditComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ SubredditComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SubredditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
