25b914d71efe3bcbdc527c561c30a7f1bae5f14b
Ernest <perkowski.ernest98@gmail.com>
2020-10-12 16:09:50 +0200

    feat(thread): create service for fetching thread

---
+++ src/app/services/thread.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThreadService {
+  baseUrl = 'https://www.reddit.com'
+
+  constructor(private http:HttpClient) { }
+
+  getData(permalink: string):Observable<any> {
+    return this.http.get<any>(this.baseUrl + permalink + ".json");
+  }
+}
--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ThreadService } from '../services/thread.service';
 
 @Component({
   selector: 'app-thread',
@@ -6,12 +7,17 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./thread.component.scss']
 })
 export class ThreadComponent implements OnInit {
-  @Input() permalink: string = '';
+  @Input() permalink: string | null = null;
   @Output() showThread = new EventEmitter<boolean>();
+  thread: any = [];
 
-  constructor() { }
+  constructor(private postService:ThreadService) { }
 
   ngOnInit(): void {
+    this.postService.getData(this.permalink).subscribe(response => {
+      this.thread = response;
+      console.log(this.thread)
+    });
   }
 
   closeThread() {
